2d5246cd61fa2a7087bcc464eb88f3b238a43d75
kurumayusamugariko <junmomo6780@gmail.com>
2023-10-08 20:36:01 +0900

    ホーム画面

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -10,14 +10,14 @@ export default function Home() {
 			<main>
 				<div className="wall">
 					<div className='Pad'>
-							<button><img src="/pad1.png" alt="gacha" className="pad1"></img><punibtn /></button>
-							<button><img src="/pad2.png" alt="2" className="pad2"></img></button>
+							<button><img src="/pad1.png" alt="config" className="pad1"></img><punibtn /></button>
+							<button><img src="/pad2.png" alt="positive-note" className="pad2"></img></button>
 							<button><img src="/pad3.png" alt="posimonbook" className="pad3"></img></button>
-							<button><img src="/pad4.png" alt="4" className="pad4"></img></button>
-							<button><img src="/pad5.png" alt="positive" className="pad5"></img></button>
+							<button><img src="/pad4.png" alt="gacha" className="pad4"></img></button>
+							<button><img src="/pad5.png" alt="positive-game" className="pad5"></img></button>
 
 					</div>
-					<img src="posita.png" alt="ぽじた/posimon" className='posimon'></img>
+					<img src="kotatu2.png" alt="posimon" className='posimon'></img>
 				</div>
 				<div className ="floor">
 
