df6dfed0b955905efe4c2632c23f80185a56bf32
mikeneko1224 <amiba725@gmail.com>
2023-10-10 18:27:54 +0900

    changed

--- frontend/src/app/posimon-book/page.js
+++ frontend/src/app/posimon-book/page.js
@@ -1,28 +1,51 @@
-import './layout.css'
+//import './layout.css'
 import Image from "next/image"
 
 export default function Home() {
     return(
-        <div>
-            <header> ぽじもん図鑑 </header>
-            <main>
-                <div class = "wall">
-                    <div class="l-wrapper_06">
-                        <div class="card_06">
+        <body>
+			<header>
+                <div className="title">
+                    ぽじもん図鑑
+                </div>
+			</header>
+			<main>
+				<div className="wall">
+                    <div className="l-wrapper_06">
+                        <div className="card_06">
                             <Image src="" alt="" />
                         </div>
                     </div>
+				</div>
                 
-                    <div className="flex">
-                        <div>あいうえお</div>
-                        <div>かきくけこ</div>
-                        <div>さしすせそ</div>
-                    </div>
-                </div>
-                <div class = "floor">
+				<div className ="floor">
 
-                </div>
-            </main>
-        </div>
+				</div>
+			</main>
+			<footer>
+
+			</footer>
+		</body>
+        // <div>
+        //     <header> ぽじもん図鑑 </header>
+        //     <main>
+        //         <div className = "wall">
+        //             <div className="l-wrapper_06">
+        //                 <div className="card_06">
+        //                     <Image src="" alt="" />
+        //                 </div>
+        //             </div>
+                
+        //             <div className="flex">
+        //                 <div>あいうえお</div>
+        //                 <div>かきくけこ</div>
+        //                 <div>さしすせそ</div>
+        //             </div>
+        //         </div>
+        //         <div className = "floor">
+        //             ああああ
+        //         </div>
+        //     </main>
+        // </div>
     )
 }
