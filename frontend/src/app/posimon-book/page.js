18d50f067c50a4f385865a4f1055f2e834269e86
mikeneko1224 <amiba725@gmail.com>
2023-10-10 16:59:32 +0900

    changed

--- frontend/src/app/posimon-book/page.js
+++ frontend/src/app/posimon-book/page.js
@@ -1,16 +1,27 @@
+import './layout.css'
+import Image from "next/image"
+
 export default function Home() {
     return(
         <div>
-            <header> </header>
+            <header> ぽじもん図鑑 </header>
             <main>
                 <div class = "wall">
+                    <div class="l-wrapper_06">
+                        <div class="card_06">
+                            <Image src="" alt="" />
+                        </div>
+                    </div>
+                
                     <div className="flex">
                         <div>あいうえお</div>
                         <div>かきくけこ</div>
                         <div>さしすせそ</div>
                     </div>
                 </div>
-                <div class = "floor"></div>
+                <div class = "floor">
+
+                </div>
             </main>
         </div>
     )
